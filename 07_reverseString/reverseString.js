b3931a099b74461b709fdacd29c100ffba4342da
noc <212793629+noc03@users.noreply.github.com>
2025-09-22 01:30:12 -0500

    finish 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(wrd) {
+    return wrd.split("").reverse().join("");
 };
+
+console.log("hola".split("").reverse().join(""))
 
 // Do not edit below this line
 module.exports = reverseString;
