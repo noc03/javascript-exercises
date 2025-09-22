bd860d12405fff221e8efc00ad8c874169eafc4f
noc <212793629+noc03@users.noreply.github.com>
2025-09-22 00:58:31 -0500

    finish 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(wrd, times) {
+    let newWrd = "";
+    if (times < 0) return "ERROR";
+    for (let i = 0; i < times; i++) {
+         newWrd = newWrd + wrd;
+    }
+    return newWrd;
 };
+console.log(repeatString("hola", -1))
 
 // Do not edit below this line
 module.exports = repeatString;
