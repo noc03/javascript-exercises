f15ea6d0d9f8d96b960f32c4ed856879e877df4b
noc <212793629+noc03@users.noreply.github.com>
2025-09-28 22:30:24 -0500

    finish 09, 10 and 11 javascript-exercises, 09-10

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) && Number.isInteger(num2)
+    && num1 > 0 && num2 > 0) {
+        if (num2 < num1) {
+            let ch = num1;
+            num1 = num2;
+            num2 = ch;
+        }
+        let accum = 0
+        for (let i = num1; i <= num2; i++) {
+            accum += i;
+        }
+        return accum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
