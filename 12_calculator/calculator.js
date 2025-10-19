00eae32c438aa494daef1c252403c4996417445f
noc <212793629+noc03@users.noreply.github.com>
2025-10-18 19:39:07 -0500

    finish 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accum, curr) => accum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accum, curr) => accum * curr);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0 || num == 1) {
+    return 1;
+  }
+  let accum = 1;
+  for (let i = 1; i <= num; i++) {
+    accum *= i;
+  }
+  return accum;
 };
 
 // Do not edit below this line
