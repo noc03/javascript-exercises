27ab688dc1b85d36b086fea2df2fbfd66d6dbca5
noc <212793629+noc03@users.noreply.github.com>
2025-10-19 03:40:23 -0500

    finish 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let age = people.map(person => {
+        if (!person.hasOwnProperty('yearOfDeath')) {
+            let currYear = new Date();
+            person.yearOfDeath = currYear.getFullYear();
+        }
+        return { name: person.name, age: (person.yearOfDeath - person.yearOfBirth)}
+    });
+    let order = age.sort((a, b) => {
+        return b.age - a.age;
+    });
+    return order[0];
 };
 
 // Do not edit below this line
