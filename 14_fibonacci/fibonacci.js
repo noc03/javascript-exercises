fc9050a1ef71f8659305d5c7cd6589ea174db7d5
noc <212793629+noc03@users.noreply.github.com>
2025-10-19 01:51:31 -0500

    finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num == 0) return 0;
+    if (num < 0) return "OOPS";
+    let arr = [1, 1];
+    for (let i = 0; i < num; i++) {
+        let result = arr[i] + arr[i + 1];
+        arr.push(result);
+    }
+    return arr[num - 1];
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
