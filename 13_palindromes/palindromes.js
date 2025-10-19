331317e411f054136dc2131c060af755b3d91785
noc <212793629+noc03@users.noreply.github.com>
2025-10-18 21:06:56 -0500

    finish 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let original = str.replace(/[^a-zA-Z0-9\s]/g, '').replace(/ +/g, '');
+    let reverse = original.split('').reverse().join('');
+    if (original.toLowerCase() != reverse.toLowerCase()) return false;
+    return true;
 };
 
 // Do not edit below this line
